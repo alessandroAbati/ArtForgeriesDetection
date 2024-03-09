476f24478b3972b8a383110401d3dfd3270a62ce
Alessandro Abati <alessandro.abati3@gmail.com>
2024-03-09 09:54:43 +0000

    Create models.py
    Created simple CNNs models with ResNet and EfficentNet backbones for both multiclass and binary classification tasks

---
+++ models.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+from efficientnet_pytorch import EfficientNet
+
+class ResNetModel(nn.Module):
+    def __init__(self, num_classes, resnet_version='resnet18', checkpoint_path=None, binary_classification=False):
+        super(ResNetModel, self).__init__()
+        self.binary_classification = binary_classification
+        
+        self.model = getattr(models, resnet_version)(pretrained=True if checkpoint_path is None else False) # Load a pretrained ResNet model
+        
+        num_features = self.model.fc.in_features
+        self.model.fc = nn.Linear(num_features, num_classes) # Replace the classifier layer
+
+        if checkpoint_path:
+            self.load_checkpoint(checkpoint_path) # Load checkpoint file
+
+    def forward(self, x):
+        if self.binary_classification:
+            return torch.sigmoid(self.model(x))
+        else:
+            return self.model(x)
+    
+    def load_checkpoint(self, checkpoint_path):
+        weights = torch.load(checkpoint_path)
+        self.model.load_state_dict(weights)
+
+class EfficientNetModel(nn.Module):
+    def __init__(self, num_classes, efficientnet_version='efficientnet-b0', checkpoint_path=None, binary_classification=False):
+        super(EfficientNetModel, self).__init__()
+        self.binary_classification = binary_classification
+
+        if checkpoint_path is None:
+            self.model = EfficientNet.from_pretrained(efficientnet_version) # Load a pretrained EfficientNet model
+        else:
+            self.model = EfficientNet.from_name(efficientnet_version) # Load without pretrained weights
+
+        num_features = self.model._fc.in_features
+        self.model._fc = nn.Linear(num_features, num_classes) # Replace the classifier layer
+
+        if checkpoint_path:
+            self.load_checkpoint(checkpoint_path) # Load checkpoint file
+
+    def forward(self, x):
+        if self.binary_classification:
+            return torch.sigmoid(self.model(x))
+        else:
+            return self.model(x)
+    
+    def load_checkpoint(self, checkpoint_path):
+        weights = torch.load(checkpoint_path)
+        self.model.load_state_dict(weights)
